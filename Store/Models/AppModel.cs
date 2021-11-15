186bad43ccb0826d63cf15b9553e89b4a469a86a
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2021-11-15 09:02:50 +0100

    chore(repo): Initial commit.

---
+++ Store/Models/AppModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Management.Deployment;
+
+namespace Store.Models
+{
+    class AppModel
+    {
+        public AppModel() {
+            // TODO
+            this.Title = "Store";
+            this.Author = "Filiph Sandström";
+            this.Description = "The best third-party/alternative store for Windows 10 Mobile.";
+            this.LogoUrl = "https://www.shareicon.net/data/2015/07/26/75452_windows_512x512.png";
+            this.Version = new Version(1, 0, 0, 0);
+            this.Dependencies = new List<String>();
+        }
+
+        public Version Version { get; }
+        public String Title { get; }
+        public String Author { get; }
+        public String Description { get; }
+        public String LogoUrl { get; }
+        public List<String> Dependencies { get; }
+
+        public async void Install() {
+            var installer = new PackageManager();
+            var options = new DeploymentOptions();
+
+            await installer.AddPackageAsync(new Uri(""), null, options);
+        }
+    }
+}
---
+++ Store/Pages/AppPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Media.Imaging;
+using Store.Models;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Store.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class AppPage : Page
+    {
+        public AppPage()
+        {
+            this.InitializeComponent();
+
+            var app = new AppModel(); 
+
+            PrimaryPivot.Title = app.Title;
+            AppNameStr.Text = app.Title;
+            AppAuthorStr.Text = app.Author;
+            AppVersionStr.Text = app.Version.ToString();
+            AppDescStr.Text = app.Description;
+            AppImg.Source = new BitmapImage(new Uri(app.LogoUrl));
+        }
+
+        private void InstallBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO
+        }
+    }
+}
